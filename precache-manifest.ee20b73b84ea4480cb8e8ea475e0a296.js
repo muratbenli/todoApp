c470618b22328d11ae27e9247b273f4b25e1c3cf
murat.benli <murat.benli@bilgeadam.com>
2020-08-05 13:53:53 +0300

    Updates

---
+++ precache-manifest.ee20b73b84ea4480cb8e8ea475e0a296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0964edf14d2020525ab3bfbceeda0ba5",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "8d63e0376bdca96bac90",
+    "url": "/todoApp/static/css/main.f828378a.chunk.css"
+  },
+  {
+    "revision": "097ac8f6c7ce92c7c67a",
+    "url": "/todoApp/static/js/2.cd6dfcb8.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/todoApp/static/js/2.cd6dfcb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d63e0376bdca96bac90",
+    "url": "/todoApp/static/js/main.920082fe.chunk.js"
+  },
+  {
+    "revision": "9028c966e44e4edab0d2",
+    "url": "/todoApp/static/js/runtime-main.3c411f8c.js"
+  }
+]);
